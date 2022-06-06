adf53a0e8d6b959ef50f672906d6a1077b9d26f7
José Rafael <81492148+JosRafael@users.noreply.github.com>
2022-06-06 14:13:19 -0300

    Add files via upload

--- Agenda/src/agenda/App.java
+++ Agenda/src/agenda/App.java
@@ -5,13 +5,41 @@ public class App {
 	public static void main(String[] args) throws Exception{
 	
 	ContatosDAO contatosDAO = new ContatosDAO();
-	Contatos contatos = new Contatos();
+	Contatos contato = new Contatos();
 	
-	contatos.setNome("Contato 001");
-	contatos.setIdade(10);
-	contatos.setDataCadastro("2022-05-24");
+	contato.setId(10);
+	contatosDAO.excluir(contato);
 	
-	contatosDAO.salvar(contatos);
+	
+	
+	
+	
+	
+	/*c.setNome("teste atualiza");
+	c.setIdade(50);
+	c.setDataCadastro("2022-06-01");
+	c.setId(10);
+	contatosDAO.atualizar(c);
+	*/
+	
+	/*
+	c.setNome("CONTATO AULA 02");
+	c.setIdade(35);
+	c.setDataCadastro("2022-06-06");
+
+	contatosDAO.salvar(c);
+	*/
+	
+		
+		
+	/*
+	Contatos contatos2 = new Contatos();
+	contatos2.setNome("Contato 001");
+	contatos2.setIdade(10);
+	contatos2.setDataCadastro("2022-05-24");
+	
+	contatosDAO.salvar(contatos2);
+	*/
 	}
 
 }
--- Agenda/src/agenda/Conexao.java
+++ Agenda/src/agenda/Conexao.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class Conexao  {
 	public static Connection ConexaoMySQL () throws Exception {
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/agenda", "root", "admin");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/agenda", "root", "alunoif21");
 		return con;
 	}
 }
